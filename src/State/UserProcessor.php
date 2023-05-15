cf8cac0c92a2e577e58b8ae001c7b6b71805ad7c
Amélia Massot <ameliamassot@gmail.com>
2023-05-15 20:06:24 +0200

    Adding the relationship between customer and user

---
+++ src/State/UserProcessor.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Metadata\DeleteOperationInterface;
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProcessorInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+
+final class UserProcessor implements ProcessorInterface
+{
+    public function __construct(
+        private readonly ProcessorInterface $persistProcessor,
+        private readonly ProcessorInterface $removeProcessor,
+        private readonly Security $security,
+    )
+    {
+    }
+
+    public function process($data, Operation $operation, array $uriVariables = [], array $context = [])
+    {
+        if ($operation instanceof DeleteOperationInterface) {
+            return $this->removeProcessor->process($data, $operation, $uriVariables, $context);
+        }
+
+        $data->setClient($this->security->getUser());
+        return $this->persistProcessor->process($data, $operation, $uriVariables, $context);
+    }
+}
