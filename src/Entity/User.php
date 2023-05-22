dcc0e9093b53241ac9f2ff00334871f238104674
Amélia Massot <ameliamassot@gmail.com>
2023-05-22 17:32:01 +0200

    Modification groups in Entity User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,14 +2,11 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Elasticsearch\Filter\TermFilter;
-use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Post;
-use App\Doctrine\CurrentClientExtension;
 use App\Repository\UserRepository;
 use App\State\UserProcessor;
 use Doctrine\DBAL\Types\Types;
@@ -20,41 +17,40 @@ use Symfony\Component\Serializer\Annotation\Groups;
     operations: [
         new Get(),
         new GetCollection(),
-        new Post(),
         new Delete(),
     ],
-    normalizationContext: ['groups' => ['read:User']],
-    denormalizationContext: ['groups' => ['write:User']],
-    paginationItemsPerPage: 5,
+    normalizationContext: ['groups' => ['read']],
+    paginationItemsPerPage: 10,
 )]
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-#[Post(processor: UserProcessor::class)]
+#[Post(denormalizationContext: ['groups' => ['write']], processor: UserProcessor::class)]
 class User
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['read:User', 'write:User'])]
+    #[Groups(['read', 'write'])]
     private ?string $firstName = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['read:User', 'write:User'])]
+    #[Groups(['read', 'write'])]
     private ?string $lastName = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['read:User', 'write:User'])]
+    #[Groups(['read', 'write'])]
     private ?string $email = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['read:User', 'write:User'])]
+    #[Groups(['read', 'write'])]
     private ?string $picture = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
-    #[Groups(['read:User'])]
+    #[Groups(['read'])]
     private ?\DateTimeInterface $creationDate = null;
 
     #[ORM\ManyToOne(cascade: ["persist"], inversedBy: 'user')]
